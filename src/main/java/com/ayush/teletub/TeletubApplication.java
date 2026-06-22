50f5480633fe11874fd824bf6407bef4af7491fb
Ayush Goyal <ayushgoyalpro@gmail.com>
2026-06-22 12:32:28 +0100

    init

---
+++ src/main/java/com/ayush/teletub/TeletubApplication.java
@@ -0,0 +1,13 @@
+package com.ayush.teletub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeletubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeletubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ayush/teletub/TeletubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayush.teletub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeletubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
