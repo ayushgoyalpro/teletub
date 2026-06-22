01b59a8daefc85d4e4bb2aa7207e837f081cc3c2
Ayush Goyal <ayushgoyalpro@gmail.com>
2026-06-22 12:33:36 +0100

    init

--- src/test/java/com/ayush/teletub/ApplicationTests.java
+++ src/test/java/com/ayush/teletub/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TeletubApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
